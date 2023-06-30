11791f9b2cd0f4f57549526625db2ba320ca82b5
richfjiq <rfjiq1986@gmail.com>
2023-06-29 18:15:29 -0600

    method decorator

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@
 // import { pokemons } from './bases/02-objects';
 // import { charmander } from './bases/03-classes';
 // import { charmander } from './bases/04-injection';
-import { charmander } from './bases/05-decorators';
+// import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
